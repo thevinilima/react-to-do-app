4ab8205ac27cbb28dc6156f2c71bdbae8813d226
Vinicius Lima <thelimavinicius@gmail.com>
2022-02-13 20:41:48 -0300

    Double quote to single quote

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,5 +1,5 @@
-import { useContext } from "react"
-import TasksContext from "../contexts/TasksContext"
+import { useContext } from 'react'
+import TasksContext from '../contexts/TasksContext'
 
 export function useTasks() {
   const context = useContext(TasksContext)
