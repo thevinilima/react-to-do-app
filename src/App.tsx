920a33a799d4554e6ee755cb0d8723dc6c81d8cd
Vinicius Lima <thelimavinicius@gmail.com>
2022-02-13 21:32:02 -0300

    GH Pages: HashRouter instead of BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { TasksProvider } from './contexts/TasksContext'
 import Home from './pages/Home'
 
