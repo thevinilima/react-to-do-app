b5c5c82157a8c3dce885486cdcbea907c00a0ae5
Vinicius Lima <thelimavinicius@gmail.com>
2022-02-13 21:30:11 -0300

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import { TasksProvider } from './contexts/TasksContext'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <TasksProvider>
         <Routes>
           <Route
@@ -14,7 +14,7 @@ function App() {
           />
         </Routes>
       </TasksProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
